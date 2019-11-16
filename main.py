4b8659ca4306ddc15d674203dcf89b5cc921975f
raspi0124 <raspi0124@gmail.com>
2019-11-16 16:34:46 +0900

    Added debug

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ def top():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 	msgtext = event.message.text
+	print(msgtext)
 	try:
 		rawdate = dparser.parse(msgtext,fuzzy=True)
 		date = rawdate.strftime('%m月%d日')
@@ -57,8 +58,10 @@ def handle_message(event):
 		tomorrow = today + timedelta(days=1)
 		if "明日" in msgtext:
 			date = datetime.strftime(tomorrow, '%m月%d日')
+			print("todd")
 		if "今日" in msgtext:
 			date = datetime.strftime(today, '%m月%d日')
+			print("tomd")
 		else:
 			return ""
 	if "欠席" in msgtext or "休む" in msgtext or "行かない" in msgtext or "行けない" in msgtext and date is not None:
